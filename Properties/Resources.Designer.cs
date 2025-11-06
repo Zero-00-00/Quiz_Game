b006768d2344bde9f0e553ec929c1fea785b38b3
BryzeOG <119498331+BryzeOG@users.noreply.github.com>
2025-11-06 09:58:36 +0530

    Add files via upload

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Quiz_Game.Properties
-{
-
-
+namespace Quiz_Game.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -19,52 +19,65 @@ namespace Quiz_Game.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Quiz_Game.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap neon_quiz_game_template_four_options_answers_for_knowledge_exam_in_school_tv_show_illustration_10_eps_ {
+            get {
+                object obj = ResourceManager.GetObject("neon-quiz-game-template-four-options-answers-for-knowledge-exam-in-school-tv-show" +
+                        "-illustration-10-eps-", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap tile {
+            get {
+                object obj = ResourceManager.GetObject("tile", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
---
+++ Quiz Master.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Quiz_Game
+{
+    public partial class Form1 : Form
+    {
+        bool will_check = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string difficulty = "";
+                decimal customTime = 0;
+                if (comboBox1.SelectedItem != null)
+                {
+                    difficulty = comboBox1.SelectedItem.ToString();
+                }
+                else
+                {
+                    label1.Text = "Please select at least one topic and a difficulty level";
+                    return; // stop execution
+                }
+                decimal questions = numericUpDown1.Value;
+                bool timed = false, neg_mark = false;
+
+                if (checkBox4.Checked) timed = true;
+                if (radioButton1.Checked) customTime = numericUpDown2.Value;
+                if (checkBox5.Checked) neg_mark = true;
+
+                List<string> selectedTopics = new List<string>();
+                if (checkBox8.Checked) selectedTopics.Add("Science");
+                if (checkBox7.Checked) selectedTopics.Add("Math");
+                if (checkBox6.Checked) selectedTopics.Add("History");
+                if (checkBox9.Checked) selectedTopics.Add("GK");
+                if (checkBox10.Checked) selectedTopics.Add("Reason");
+                if (checkBox11.Checked) selectedTopics.Add("Geo");
+
+                if (selectedTopics.Count == 0)
+                {
+                    label1.Text = "Please select at least one topic and a difficulty level.";
+                }
+                else
+                {
+                    Quiz quizForm = new Quiz(difficulty, selectedTopics, timed, neg_mark, questions, customTime);
+                    quizForm.Show();
+                    this.Hide();
+                    quizForm.FormClosed += quizForm_FormClosed;
+                }
+            }
+            catch (System.NullReferenceException)
+            {
+                label1.Text = "Please select at least one topic and a difficulty level.";
+            }
+        }
+        private void quizForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.Show();
+            //this.Close();
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Visible)
+                radioButton1.Visible = false;
+            else
+                radioButton1.Visible = true;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (numericUpDown2.Visible)
+                numericUpDown2.Visible = false;
+            else
+                numericUpDown2.Visible = true;
+        }
+
+        private void radioButton1_Click(object sender, EventArgs e)
+        {
+            if (will_check)
+            {
+                radioButton1.Checked = false;
+                will_check = false;
+            }
+            else
+            {
+                radioButton1.Checked = true;
+                will_check = true;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+    }
+}
